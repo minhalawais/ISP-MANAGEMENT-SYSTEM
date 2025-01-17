0bc7d9af2e8418245cbc5f7c2bbba4d21c834679
Minhal Awais <105672263+minhalawais@users.noreply.github.com>
2025-01-18 02:57:16 +0500

    Update config.py

--- api/config.py
+++ api/config.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:M.m03007493358@localhost:5432/isp_management'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Mba.net0614727@localhost:5432/isp_management'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'your_jwt_secret_key'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
