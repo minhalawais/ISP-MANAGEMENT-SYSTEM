2b03f3d05ea42e4cdb1056957689e3db464e2679
Minhal Awais <105672263+minhalawais@users.noreply.github.com>
2025-01-21 03:00:03 +0500

    Add files via upload

--- api/config.py
+++ api/config.py
@@ -1,4 +1,5 @@
 from datetime import timedelta  
+import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'
@@ -8,3 +9,9 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     DEBUG=True
     ENV="development"
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
